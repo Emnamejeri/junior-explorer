bc16098fb5160286d3b3bd66a4f30da169799632
emna mj <107746809+Emnamejeri@users.noreply.github.com>
2023-10-01 10:53:02 +0300

    component update

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quos
+    nobis odit, laudantium unde adipisci commodi vero expedita dolor.
+    Exercitationem.
+  </p>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    msg: String,
+  },
+})
+export default class HelloWorld extends Vue {
+  msg!: string;
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/QuizQuestions.vue
@@ -0,0 +1,2 @@
+<template><h1>test test 123</h1></template>
+
